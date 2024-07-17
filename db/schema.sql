c27c5969e3c0193171ea09ad51147657544ed4f0
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-07-17 13:41:02 -0400

    updated schema, added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,9 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,12 +22,21 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
     manager_id INTEGER
+    FOREIGN KEY (manager_id)
+    REFERENCES manager(id)
 );
 
 CREATE TABLE manager (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    department_id INTEGER
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    role_id INTEGER NOT NULL.
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
